2da75d399e8e5af762553e32813a2b3cac371270
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-30 16:12:16 +0100

    Mostro i 5 numeri

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@
 
 // - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri 
 //   da indovinare sono stati individuati.
+
+
+
+
+// Visualizzo 5 alert con numeri da 1 a 100.
+for ( let i = 0; i < 5; i++ ) {
+    casualNumber = Math.floor(Math.random() * 100) + 1
+    alert(`Numero: ${casualNumber}`)
+}
+
+// Dopo i 30 secondi , l'utente inserisce un numero alla volta attraverso il prompt
+
+
+// stampo successivamente quanti numeri e quali ha indovinato l'utente.
+
+
+
